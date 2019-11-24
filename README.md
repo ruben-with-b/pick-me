7d1aee02c2cac8364eba143d235a7bddbf42001c
loderer <rudi.loderer@hs-augsburg.de>
2019-11-24 13:34:45 +0100

    Start backend and frontend concurrently.

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # Entwickler-Dokumentation
+
+## Start
+Zum Starten der Anwendung `npm start` eingeben. Danach findet man die Anwendung
+unter *https://localhost:8080*.<br>
+
+## Konsolen-Output
+Jede Zeile, die mit [0] beginnt stammt vom Backend.<br>
+Analog dazu stammt jede Zeile, die mit [1] beginnt vom Frontend.
+
+Um den Konsolen-Output beider Teile zu trennen kann man in zwei verschiedenen 
+Terminals `npm run start:frontend` und `npm run start:backend` ausführen.
+
 ## Linter
 In diesem Projekt wird [ESLint](https://eslint.org/) als Linter verwendet. 
 Bitte konfiguriere deine IDE so, dass sie die Warnungen und Fehler von 
---
+++ webpack.config.js
@@ -0,0 +1,103 @@
+const path = require('path');
+
+const {VueLoaderPlugin} = require('vue-loader');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+module.exports = (env, argv) => ({
+  mode: argv && argv.mode || 'development',
+  devtool: (argv && argv.mode || 'development') === 'production' ?
+      'source-map' : 'eval',
+
+  entry: './frontend/src/app.js',
+
+  output: {
+    path: path.resolve(__dirname, 'frontend', 'dist'),
+    filename: '[name].js',
+  },
+
+  node: false,
+
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+        ],
+        exclude: /\.module\.css$/,
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: [
+      '.js',
+      '.vue',
+      '.json',
+    ],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+      '@': path.resolve(__dirname, 'frontend', 'src'),
+    },
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'frontend', 'static', 'index.html'),
+      inject: true,
+    }),
+    new CopyWebpackPlugin([{
+      from: path.resolve(__dirname, 'frontend', 'static'),
+      to: path.resolve(__dirname, 'frontend', 'dist'),
+      toType: 'dir',
+    }]),
+  ],
+
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      minSize: 30000,
+      maxSize: 0,
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
+    runtimeChunk: {
+      name: (entrypoint) => `runtime~${entrypoint.name}`,
+    },
+    mangleWasmImports: true,
+    removeAvailableModules: true,
+    removeEmptyChunks: true,
+    mergeDuplicateChunks: true,
+  },
+
+  devServer: {
+    compress: true,
+    host: 'localhost',
+    https: true,
+    open: true,
+    overlay: true,
+    port: 8080,
+  },
+});
