b6a76fa6a4c2d5c5223e8c264ea25ef039b499be
loderer <rudi.loderer@hs-augsburg.de>
2019-11-24 13:39:10 +0100

    Fix some linter-problems

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,31 +1,31 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from 'vue';
+import Router from 'vue-router';
 
 // Page content
-import Overview from '@/components/Overview'
-import Dashboard from '@/components/Dashboard'
+import Overview from '@/components/Overview';
+import Dashboard from '@/components/Dashboard';
 
 // Fallback page
-import PageNotFound from '@/components/PageNotFound'
+import PageNotFound from '@/components/PageNotFound';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
       path: '/',
       name: 'Dashboard',
-      component: Dashboard
+      component: Dashboard,
     },
     {
       path: '/overview',
       name: 'Overview',
-      component: Overview
+      component: Overview,
     },
     {
       path: '**',
       name: 'PageNotFound',
-      component: PageNotFound
-    }
-  ]
-})
+      component: PageNotFound,
+    },
+  ],
+});
