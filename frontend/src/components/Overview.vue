06acdd72dd211708f42f043e57c1cf8aa677ae04
rub-n <ruben.santoro@hs-augsburg.de>
2019-11-28 18:50:19 +0100

    Install hooper-slider for Bag Cards

--- frontend/src/components/Overview.vue
+++ frontend/src/components/Overview.vue
@@ -1,11 +1,58 @@
 <template>
   <div>
     <h1>Choose your bag</h1>
+    <hooper :settings="hooperSettings">
+      <slide>
+        slide 1
+      </slide>
+      <slide>
+        slide 2
+      </slide>
+      <slide>
+        slide 3
+      </slide>
+      <slide>
+        slide 4
+      </slide>
+      <slide>
+        slide 5
+      </slide>
+      <slide>
+        slide 6
+      </slide>
+    </hooper>
   </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+.hooper{
+  height: 60vh;
+  margin: 1em 0 0 0;
+  .hooper-slide{
+    background: #fff;
+    border-radius: 1.5em;
+    margin: 0 1em;
+    box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.05);
+  }
+}
 </style>
 
 <script>
+import {Hooper, Slide} from 'hooper';
+import 'hooper/dist/hooper.css';
+
+export default {
+  name: 'App',
+  components: {
+    Hooper,
+    Slide,
+  },
+  data() {
+    return {
+      hooperSettings: {
+        itemsToShow: 1.5,
+      },
+    };
+  },
+};
 </script>
