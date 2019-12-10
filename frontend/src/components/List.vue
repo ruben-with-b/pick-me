d5811eb74b649cacf6125e2b30e969f20f29785d
rub-n <ruben.santoro@hs-augsburg.de>
2019-12-10 17:18:01 +0100

    Add items, checkboxes, close button to List

--- frontend/src/components/List.vue
+++ frontend/src/components/List.vue
@@ -5,7 +5,31 @@
         alt="backpack" class="symbol-img">
     </div>
     <div class="list-body">
-      <input :placeholder="list.name">
+      <div class="packlist-wrapper">
+        <input class="listname-input" :value="list.name">
+        <ul class="packlist">
+          <li>
+            <input type="checkbox">
+            item 1
+            <button>X</button>
+          </li>
+          <li>
+            <input type="checkbox">
+            item 2
+            <button>X</button>
+          </li>
+          <li>
+            <input type="checkbox">
+            item 3
+            <button>X</button>
+          </li>
+          <li>
+            <input type="checkbox">
+            item 4
+            <button>X</button>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -36,6 +60,34 @@
   height: 85vh;
   margin-top: -9vh;
   z-index: 10;
+
+  .packlist-wrapper{
+    width: 70%;
+
+    .listname-input{
+      margin: 2em 0 0 0;
+      width: 100%;
+      border:none;
+      border-bottom: 3px solid #fff;
+      background-image:none;
+      background-color:transparent;
+      box-shadow: none;
+      color: #fff;
+    }
+
+    .packlist{
+      list-style: none;
+      margin: 3em 0 0 0;
+      padding: 0;
+      font-size: 1.3rem;
+      line-height: 1.7rem;
+
+      button{
+        color: #fff;
+        cursor: pointer;
+      }
+    }
+  }
 }
 
 </style>
