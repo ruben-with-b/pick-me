33cd03817cac9dd6f75d3da66c7c1b66d75a0604
loderer <rudi.loderer@hs-augsburg.de>
2019-12-15 13:44:38 +0100

    Remove unused libraries/routes.

--- backend/app.js
+++ backend/app.js
@@ -1,33 +1,30 @@
+// Load environment variables
 require('dotenv').config();
 
+// Auto-generated-at-express-setup---------------------------------------------
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes');
-const bagTemplatesRouter = require('./routes/bagTemplates');
-const bagsRouter = require('./routes/bags');
-
-const NotificationScheduler =
-  require('./libs/pickme/logic/NotificationScheduler');
-
-// import swagger
-const yaml = require('yamljs');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = yaml.load('./backend/swagger.yaml');
-
 const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// ----------------------------------------------------------------------------
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+const NotificationScheduler =
+  require('./libs/pickme/logic/NotificationScheduler');
+// Import routes
+const bagTemplatesRouter = require('./routes/bagTemplates');
+const bagsRouter = require('./routes/bags');
+// Import modules relevant for api-documentation.
+const yaml = require('yamljs');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = yaml.load('./backend/swagger.yaml');
 
 // Enable deploying the frontend and backend with two different ports at the
 // same machine.
@@ -37,12 +34,12 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.use('/', indexRouter);
+// Add routes.
 app.use('/bag_templates', bagTemplatesRouter);
 app.use('/my_bags', bagsRouter);
-
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+// Init notifications.
 NotificationScheduler.init().then(() => {
   console.log('Notification scheduler successfully initialized');
 }).catch((e) => {
